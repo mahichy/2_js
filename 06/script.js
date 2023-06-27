e91682d99b1503105d4a1f9a6735fbe9dcabec96
mahichy <mohiuddinoshim@yahoo.com>
2023-06-27 08:15:56 +0600

    Variable and datatype

---
+++ 06/script.js
@@ -0,0 +1,13 @@
+var firstName = 'Mahi';
+console.log(firstName);
+
+var lastName = 'Chowdhury';
+console.log(lastName)
+
+var age = 28;
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job)
