d2a59b9db378ab46d9fb9af17d61d80c377ccd41
mahichy <mohiuddinoshim@yahoo.com>
2023-06-28 00:02:44 +0600

    Basic Operators

---
+++ 08/script.js
@@ -0,0 +1,98 @@
+
+// var firstName = 'Mahi';
+// console.log(firstName);
+
+// var lastName = 'Chowdhury';
+// console.log(lastName)
+
+// var age = 28;
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job)
+
+/***************************************
+ * Variable Mutation and type coercion
+ */
+
+/*
+var firstName = 'Mahi';
+var age = 30;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + 'years old ' + job + '. is he married? ' + isMarried);
+
+// Variable Mutation
+
+age = "Twenty eight";
+job = "Driver";
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. is he married? ' + isMarried);
+
+var lastName = prompt('What is your last name');
+console.log(firstName + ' ' + lastName);
+*/
+
+// Class_08
+
+/********************************
+ * Basic Operators
+ */
+
+var now, yearJhon, yearMark;
+now = 2023;
+ageJhon = 28;
+ageMark = 35
+// Math operators
+yearJhon = now - ageJhon;
+yearMark = now - ageMark;
+
+console.log(yearJhon);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+
+// Logical Operators
+
+var jhonOlder = ageJhon > ageMark;
+console.log(jhonOlder);
+
+var jhonOlder = ageJhon < ageMark;
+console.log(jhonOlder);
+
+// Typeof operator
+
+console.log(typeof jhonOlder);
+console.log(typeof ageJhon);
+
+var x;
+console.log(typeof x);
+console.log(typeof 'this is mahi')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
